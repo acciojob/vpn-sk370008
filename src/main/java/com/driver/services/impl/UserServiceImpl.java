d7f3017a0da2b9e7577eb629329d51c9e12be39a
Shoaib Akhtar <sk370008@gmail.com>
2023-04-21 11:37:39 +0530

    fix

--- src/main/java/com/driver/services/impl/UserServiceImpl.java
+++ src/main/java/com/driver/services/impl/UserServiceImpl.java
@@ -116,6 +116,7 @@ public class UserServiceImpl implements UserService {
         User user = new User();
         user.setUsername(username);
         user.setPassword(password);
+        user.setConnected(false);
         country.setUser(user);
         user.setOriginalCountry(country);
         user = userRepository3.save(user);
